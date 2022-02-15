96e37c7a9a2be5390210db228b813389acfd1347
Leon2ev <jevgeni.glazunov@gmail.com>
2022-02-15 15:57:11 +0100

    Update some packages. Add Readme. Added Pinia.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import PrimeVue from 'primevue/config'
+import { createPinia } from 'pinia'
 
 // Styles
 import 'primevue/resources/themes/saga-blue/theme.css'
@@ -11,6 +12,8 @@ import 'primeflex/primeflex.css'
 import ToastService from 'primevue/toastservice'
 
 const app = createApp(App)
+
+app.use(createPinia())
 
 app.use(PrimeVue)
 app.use(ToastService)
